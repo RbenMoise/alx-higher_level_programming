ec5bb795fd0b61e7ed03e84d30c868c306cb5d95
RbenMoise <reubenmoses91@gmail.com>
2023-10-03 16:55:23 -0700

    all

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+for char_code in range(ord('z'), ord('A') - 1, -1):
+    char = chr(char_code)
+    if char.islower():
+        print("{}".format(char, end="")
+    elif char.isupper():
+        print("{}".format(char, end="")
+
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        if 'a' <= str <= 'z':
-           char = chr(ord(char)-32)
+        if 'a' <= char <= 'z':
+            char = chr(ord(char)-32)
         print("{}".format(char), end="")
     print("")
