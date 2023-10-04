fa001a575f03ac300db8d2d859775870a97d1f55
RbenMoise <reubenmoses91@gmail.com>
2023-10-04 00:42:40 -0700

    A

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,6 @@
 for char_code in range(ord('z'), ord('A') - 1, -1):
     char = chr(char_code)
     if char.islower():
-        print("{}".format(char, end="")
+        print("{}".format(char), end="")
     elif char.isupper():
-        print("{}".format(char, end="")
-
+        print("{}".format(char), end="")
