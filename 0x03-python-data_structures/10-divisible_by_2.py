a89b66a918589e13d8cd3e0032886589eab4e947
RbenMoise <reubenmoses91@gmail.com>
2023-10-09 11:54:19 -0700

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/user/bin/python3
+def divisible_by_2(my_list=[]):
+    div = []
+    for i in my_list:
+        if i % 2 == 0:
+            div.append(True)
+        else:
+            div.append(False)
+    return (div)
