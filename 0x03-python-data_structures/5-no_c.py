46c2682b4725f32d0a2e5390ac6f863b17fa8592
RbenMoise <reubenmoses91@gmail.com>
2023-10-09 07:05:47 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,6 @@
 def no_c(my_string):
     new_string = ""
     for ch in my_string:
-        if ch =! c and ch +! C:
-            :wq
+        if ch != 'c' and ch != 'C':
             new_string += ch
     return new_string
