da68c75e78503235c8f97094ef5cf9c792a06923
RbenMoise <reubenmoses91@gmail.com>
2023-10-12 06:14:38 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list is None:
-        return
-    new = my_list[:]
-    for item, n in enumerate(new):
-        if n == search:
-            new[item] = replace
-        return (new)
+    new = list(map(lambda x: replace if x == search else x, my_list))
+    return (new)
