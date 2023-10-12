1f14c3e409aee253e535ccf6088556627cdce9b5
RbenMoise <reubenmoses91@gmail.com>
2023-10-12 03:20:09 -0700

    102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for key in list(a_dictionary):
-    if a_dictionary[key] == value:
-        del a_dictionary[key]
+def complex_delete(a_dictionary, value):
+    for key in list(a_dictionary):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
     return (a_dictionary)
